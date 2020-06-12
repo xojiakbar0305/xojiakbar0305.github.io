96c028303e4f265266e3327d4286f3e5326c997d
xojiakbar0305 <xojiakbar.shukurov@yandex.ru>
2020-06-12 17:51:13 +0500

    yangilandi

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const backToTopButton = document.querySelector(".scroll-up__boxlink")
+
+window.addEventListener("scroll", scrollFunction)
+
+function scrollFunction() {
+	if (window.pageYOffset > 300) {
+		if (!backToTopButton.classList.contains("btnEntranse")) {
+			backToTopButton.classList.remove("btnExit");
+			backToTopButton.classList.add("btnEntranse");
+			backToTopButton.style.display = "block";
+		}
+	}
+	else {
+		if (backToTopButton.classList.contains("btnEntranse")) {
+			backToTopButton.classList.remove("btnEntranse");
+			backToTopButton.classList.add("btnExit");
+			setTimeout(function() {
+				backToTopButton.style.display = "none";
+			}, 250);
+		}
+	}
+}
+
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function backToTop () {
+	window.scrollTo(0, 0);
+}
+
+function smoothScrollBackToTop() {
+	const targetPosition = 0;
+	const startPosition = window.pageYOffset;
+	const distance = targetPosition - startPosition;
+	const duration = 750;
+	let start = null;
+
+	window.requestAnimationFrame(step);
+
+	function step(timestamp) {
+		if (!start) start = timestamp;
+		const progress = timestamp - start;
+		window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+		if (progress < duration) window.requestAnimationFrame(step);
+	}
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t*t + b;
+	t -= 2;
+	return c/2*(t*t*t + 2) + b;
+};
